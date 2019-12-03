a91bce5243b6a2246d0b1c3a6b42e8a0dae97406
Student <example@example.com>
2019-12-03 10:38:12 -0800

    connect front end with backend in policy selector.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-// import { Object } from  './backend.js';
+import { Game, Players } from  './backend.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
+const game = new Game();
+const players = new Players();
 const discardArray = [];
 
 
@@ -33,6 +34,8 @@ $(document).ready(function() {
     player3 = $("input#player3").val();
     player4 = $("input#player4").val();
     player5 = $("input#player5").val();
+    game.playerOrder.push(player1, player2, player3, player4, player5);
+    console.log(game.playerOrder);
     $("#player1Name").text(player1);
     $("#player2Name").text(player2);
     $("#player3Name").text(player3);
@@ -49,12 +52,14 @@ $(document).ready(function() {
 
   $("form.form6").submit(function(event) {
     event.preventDefault();
-    player1 =  $("input#player1").val();
-    player2 = $("input#player2").val();
-    player3 = $("input#player3").val();
-    player4 = $("input#player4").val();
-    player5 = $("input#player5").val();
-    player6 = $("input#player6").val();
+    player1 =  $("input#player1b").val();
+    player2 = $("input#player2b").val();
+    player3 = $("input#player3b").val();
+    player4 = $("input#player4b").val();
+    player5 = $("input#player5b").val();
+    player6 = $("input#player6b").val();
+    game.playerOrder.push(player1, player2, player3, player4, player5, player6);
+    console.log(game.playerOrder);
     $("#player1Name").text(player1);
     $("#player2Name").text(player2);
     $("#player3Name").text(player3);
@@ -124,14 +129,55 @@ $("button.nameButton").click(function() {
 $("button#presidentVerifyButton").click(function() {
   $(".presidentPolicyCheck").hide();
   $(".presidentPolicyElimination").show();
+    game.drawThreeCards();
+  $("#presidentPolicy1").text(game.drawnCardsArray[0]);
+  $("#presidentPolicy2").text(game.drawnCardsArray[1]);
+  $("#presidentPolicy3").text(game.drawnCardsArray[2]);
 })
 
-$("form.presidentPolicyForm input").click(function(e) {
-  $("#" + $(this).next()[0].id).hide();
-  discardArray.push($("#" + $(this).next()[0].id).text());
 
+$("form.presidentPolicyForm input").click(function() {
+  $(this).hide();
+  console.log($(this));
+  let badCard = $("#" + $(this).next()[0].id);
+  discardArray.push(badCard.text());
+  console.log(discardArray);
+  console.log(badCard);
   //This was with Travis' help, to get the info for the selected radio to be removed STILL NEED TO FIGURE OUT REMOVAL AND CHANCELLOR ROUND.//
+  if (badCard.index() === 1) {
+    game.drawnCardsArray.splice(0, 1);
+  } else if (badCard.index() === 3) {
+    game.drawnCardsArray.splice(1, 1);
+  } else {
+    game.drawnCardsArray.splice(2, 1);
+  }
+  console.log(discardArray);
+  $(".presidentPolicyElimination").hide();
+  $(".chancellorPolicyCheck").show();
 
 })
+
+$("#chancellorVerifyButton").click(function() {
+  $("#chancellorPolicy1").text(game.drawnCardsArray[0]);
+  $("#chancellorPolicy2").text(game.drawnCardsArray[1]);
+  $(".chancellorPolicyCheck").hide();
+  $(".chancellorPolicyElimination").show();
+})
+
+$("form.chancellorPolicyForm input").click(function() {
+  $(this).hide();
+  let badCard = $("#" + $(this).next()[0].id);
+  discardArray.push(badCard.text());
+  if (badCard.index() === 1) {
+    game.drawnCardsArray.splice(0, 1);
+  } else {
+    game.drawnCardsArray.splice(1, 1);
+  }
+  $(".chancellorPolicyElimination").hide();
+  //THIS IS WHERE WE SHOW THE CARD ON THE BOARD///
+})
+
+
+
 
 });
