ecd43337020d90620fa86632d03e2269d7179082
Student <example@example.com>
2019-12-02 16:33:02 -0800

    Assign party function working and tested good.

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,1 +1,22 @@
-// import { Object } from './../src/backend.js'
+import { Game, deckArray, Players } from './../src/backend.js'
+
+describe ('Game', () => {
+  test('should populate the deck array based on random elements from total deck', () => {
+    let players = 5
+    let game = new Game(players);
+    game.shuffleDeck(deckArray);
+    expect(game.deck.length).toEqual(17)
+  });
+  test('should assign two players a fascist and one of them as hitler', () => {
+    let player1 = new Players('Tom');
+    let player2 = new Players('Bill');
+    let player3 = new Players('Sheila');
+    let player4 = new Players('Tara');
+    let player5 = new Players('George');
+    let game = new Game(5);
+    game.playerOrder = [player1, player2, player3, player4, player5];
+    game.assignParty();
+    expect(game.playerOrder[0].party==="Fascist" || game.playerOrder[1].party==="Fascist" || game.playerOrder[2].party==="Fascist" || game.playerOrder[3].party==="Fascist" || game.playerOrder[4].party==="Fascist");
+    expect(game.playerOrder[0].secret==="Hitler" || game.playerOrder[1].secret==="Hitler" || game.playerOrder[2].secret==="Hitler" || game.playerOrder[3].secret==="Hitler" || game.playerOrder[4].secret==="Hitler");
+  })
+});
--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,98 @@
 // export class Object {
-// }
+  // }
+  export let deckArray = ['fascist1','fascist2','fascist3','fascist4','fascist5','fascist6','fascist7','fascist8','fascist9','fascist10','fascist11','liberal1','liberal2','liberal3','liberal4','liberal5','liberal6']
+  //number of players
+  //Player one sign in - looped based off of input player numbers
+  //so player 1 = chad
+
+  export class Players {
+    constructor(userName) {
+      this.userName = userName;
+      this.status = "Alive";
+      this.party = "Liberal";
+      this.secret = "";
+    }
+
+    //party assignment and secret assignment
+
+    //Random first president candidate
+
+  }
+
+
+
+  // failed election variable
+
+  // term limit array to store and restor to turn array
+
+  // turn array itself
+
+  export class Game {
+    constructor(players){
+      this.players = players;
+      this.playerOrder = []; //each element is player object
+      this.eligibleChancelors = [];
+      this.deck = [];
+      this.totalYesVote = 0;
+      this.liberalPolicies = 0;
+      this.fascistPolicies = 0;
+      this.electionTracker = 0;
+      this.legistlateCards = [];
+      this.currentChancellor = "";
+      this.currentPresident = "";
+      //Maybe have discarded cards array
+
+    }
+    shuffleDeck(deckToShuffle) {
+      let isDeckShuffled = false;
+      while (isDeckShuffled === false) {
+        let number = Math.floor(Math.random() * deckToShuffle.length);
+        let card = deckToShuffle.splice(number, 1);
+        this.deck.push(card.toString());
+        if (this.deck.length === 17) {
+          isDeckShuffled = true;
+        }
+      }
+    }
+    assignParty() {
+      let partyArray = this.playerOrder;
+      let number = Math.floor(Math.random() * partyArray.length);
+      this.playerOrder[number].party = "Fascist";
+      this.playerOrder[number].secret = "Hitler";
+      partyArray.splice[number, 1];
+      let number2 = Math.floor(Math.random() * partyArray.length);
+      if (number === number2) {
+        number2 = number2 +1;
+      }
+      this.playerOrder[number2].party = "Fascist";
+    }
+
+
+  }
+  //random 1st president
+
+  // assign party / secret cards
+
+  // president assigns chancelor
+
+  //hold the vote (check for 3 times hitler)
+
+  //pass fail vote result handler
+
+  //president legistative cards from user inputs to updating main board
+
+  //show 3 top cards
+
+  // shoot and remove player and check if hitler
+
+  // template cardshuffler
+
+  // template inelligiblechancelors
+
+  // template player to the left
+
+  //failed election tracker and function to enact
+
+  // reshuffle protocol
+
+  //check board status for wins
